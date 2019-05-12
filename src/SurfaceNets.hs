422452986a118cdd69662b3eb1885f96d32b11fd
stla <laurent_step@yahoo.fr>
2019-05-12 10:43:41 +0200

    surface nets fourth commit

--- src/SurfaceNets.hs
+++ src/SurfaceNets.hs
@@ -2,7 +2,7 @@ module SurfaceNets
   where
 import Data.Bits
 import Data.List.Index (imap)
-import Data.Sequence (Seq, (|>), adjust')
+import Data.Sequence (Seq, (|>), adjust', update, index)
 import qualified Data.Sequence as S
 import MarchingCubes.Voxel -- (Voxel)
 
@@ -38,34 +38,38 @@ edgeTable = [
 
 surfaceNets :: Voxel -> Seq Int -- Seq (Seq Double)
 surfaceNets (dat, (nx,ny,nz), ((xmin,xmax),(ymin,ymax),(zmin,zmax))) =
-  loop2 0 1 (S.empty, S.replicate (2*(nx+1)*(ny+1)) 0)
+  loop2 0 1 (S.empty, S.replicate (2*(nx+1)*(ny+1)) 0, S.empty)
   where
-  loop2 :: Int -> Int -> (Seq (Seq Double), Seq Int) -> Seq Int -- Seq (Seq Double)
-  loop2 x2 b (vs,bf) | x2 == nz-1 = bf -- vs
-                     | otherwise =
-                       loop2 (x2+1) (1-b) (loop1 0 x2 m (vs,bf))
-                         where
-                         m = 1 + (nx + 1) * (1 + b * (ny + 1))
+  loop2 :: Int -> Int -> (Seq (Seq Double), Seq Int, Seq [Int]) -> Seq Int -- Seq (Seq Double)
+  loop2 x2 b (vs,bf,fs) | x2 == nz-1 = bf -- vs
+                       | otherwise =
+                         loop2 (x2+1) (1-b) (loop1 0 x2 m (vs,bf,fs))
+                           where
+                           m = 1 + (nx + 1) * (1 + b * (ny + 1))
   -- loop1 :: Int -> Int -> Int -> Seq (Seq Double) -> Seq (Seq Double)
-  loop1 x1 x2 m (vs,bf) | x1 == ny-1 = (vs,bf)
-                        | otherwise =
-                          loop1 (x1+1) x2 (m+nx-1+2) (loop0 0 x1 x2 m (vs,bf))
-  loop0 x0 x1 x2 m (vs,bf) | x0 == nx-1 = (vs,bf)
-                           | otherwise =
-                             loop0 (x0+1) x1 x2 (m+1) (f1 vs x0 x1 x2 bf m)
-  f1 vs x0 x1 x2 bf m = (vsv, bf')
+  loop1 x1 x2 m (vs,bf,fs) | x1 == ny-1 = (vs,bf,fs)
+                          | otherwise =
+                            loop1 (x1+1) x2 (m+nx-1+2)
+                                  (loop0 0 x1 x2 m (vs,bf,fs))
+  loop0 x0 x1 x2 m (vs,bf,fs) | x0 == nx-1 = (vs,bf,fs)
+                              | otherwise =
+                                loop0 (x0+1) x1 x2 (m+1) (f1 vs fs x0 x1 x2 bf m)
+  f1 vs fs x0 x1 x2 bf m = (vsv, bf', fs')
     where
     grid = [dat !! (x0 + i + (x1 + j) * nx + (x2 + k) * nx * ny) |
             k <- [0,1], j <- [0,1], i <- [0,1]]
     is = imap (\i d -> if d<0 then shiftL (1::Int) i else (0::Int)) grid
     mask = foldr (.|.) 0 is
-    (vsv,bf') = if mask == 0 || mask == 255
+    edgeMask = edgeTable !! mask
+    (vsv,bf',fs') = if mask == 0 || mask == 255
       then
-        (vs, bf)
+        (vs, bf, fs)
       else
-        (vs |> v, S.update m (S.length vs) bf)
+        (vs |> v,
+         update m (S.length vs) bf,
+         updatef fs bf m mask edgeMask [x0,x1,x2])
       where
-        v = update2 (f2 grid (edgeTable !! mask) (S.replicate 3 0)) [x0,x1,x2]
+        v = updatevx2 (f2 grid edgeMask (S.replicate 3 0)) x0 x1 x2
   f2 :: [Double] -> Int -> Seq Double -> (Seq Double, Double)
   f2 grid edgeMask v0 = vAndEcount
     where
@@ -81,9 +85,9 @@ surfaceNets (dat, (nx,ny,nz), ((xmin,xmax),(ymin,ymax),(zmin,zmax))) =
                               oloop (i+1) (ecount+1)
                                     (f3 e0 e1 t vx)
                             where
-                            index = shiftL i 1
-                            e0 = cubeEdges !! index
-                            e1 = cubeEdges !! (index + 1)
+                            idx = shiftL i 1
+                            e0 = cubeEdges !! idx
+                            e1 = cubeEdges !! (idx + 1)
                             g0 = grid !! e0
                             g1 = grid !! e1
                             t = g0 / (g0-g1)
@@ -95,24 +99,45 @@ surfaceNets (dat, (nx,ny,nz), ((xmin,xmax),(ymin,ymax),(zmin,zmax))) =
       iloop j k vx | j == 3 = vx
                    | otherwise =
                      iloop (j+1) (shiftL k 1)
-                           (update (e0 .&. k) (e1 .&. k) vx j)
-      update a b vx j =
+                           (updatevx (e0 .&. k) (e1 .&. k) vx j)
+      updatevx a b vx j =
         if a /= b
           then
             adjust' (+ (if a>0 then 1-t else t)) j vx
           else
             adjust' (+ (if a>0 then 1 else 0)) j vx
-  update2 (v, eCount) x = v'''
+  updatevx2 (v, eCount) x0 x1 x2 = v'''
     where
       scx = (xmax - xmin) / (fromIntegral nx - 1)
       scy = (ymax - ymin) / (fromIntegral ny - 1)
       scz = (zmax - zmin) / (fromIntegral nz - 1)
-      x0 = fromIntegral (x !! 0)
-      x1 = fromIntegral (x !! 1)
-      x2 = fromIntegral (x !! 2)
-      v' = adjust' (\v0 -> scx * (x0 + v0 / eCount) + xmin) 0 v
-      v'' = adjust' (\v1 -> scy * (x1 + v1 / eCount) + ymin) 1 v'
-      v''' = adjust' (\v2 -> scz * (x2 + v2 / eCount) + zmin) 2 v''
+      x0' = fromIntegral x0
+      x1' = fromIntegral x1
+      x2' = fromIntegral x2
+      v' = adjust' (\v0 -> scx * (x0' + v0 / eCount) + xmin) 0 v
+      v'' = adjust' (\v1 -> scy * (x1' + v1 / eCount) + ymin) 1 v'
+      v''' = adjust' (\v2 -> scz * (x2' + v2 / eCount) + zmin) 2 v''
+  updatef = loop 0
+  loop i fs bf m mask edgeMask x | i==3 = fs
+                                 | otherwise =
+                                   if (edgeMask .&. shiftL 1 i) == 0 || (x !! iu) == 0 || (x !! iv) == 0
+                                     then
+                                       loop (i+1) fs bf m mask edgeMask x
+                                     else
+                                       loop (i+1) fs' bf m mask edgeMask x
+                                   where
+                                   iu = mod (i+1) 3
+                                   iv = mod (i+2) 3
+                                   du = iu -- r !! iu
+                                   dv = iv -- r !! iv
+                                   (fc1, fc2) = if (mask .&. 1) > 0
+                                     then
+                                       ([index bf m, index bf (m-du), index bf (m-dv)],
+                                        [index bf (m-dv), index bf (m-du), index bf (m-du-dv)])
+                                     else
+                                       ([index bf m, index bf (m-dv), index bf (m-du)],
+                                        [index bf (m-du), index bf (m-dv), index bf (m-du-dv)])
+                                   fs' = (fs |> fc1) |> fc2
 
 
 ftest :: (Double, Double, Double) -> Double
